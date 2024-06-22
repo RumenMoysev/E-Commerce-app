706e456f235e79491862cb0cac4d59a8ac379273
RumenMoysev <rumoysev@gmail.com>
2024-06-23 01:07:07 +0300

    Update orderItemSchema

--- src/models/Order.js
+++ src/models/Order.js
@@ -1,19 +1,19 @@
 const mongoose = require('mongoose')
 
 const OrderItemSchema = new mongoose.Schema({
-    productId: {
+    id: {
         type: String,
         required: true
     },
-    productName: {
+    name: {
         type: String,
         required: true
     },
-    productPrice: {
+    price: {
         type: Number,
         required: true
     },
-    productQuantity: {
+    quantity: {
         type: Number,
         required: true
     }
