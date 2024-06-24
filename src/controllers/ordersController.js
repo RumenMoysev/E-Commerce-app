d66d6ceb6637bb91f16b9b57a2973558825f3194
RumenMoysev <rumoysev@gmail.com>
2024-06-24 17:10:48 +0300

    Add /pay-order/:id path

--- src/controllers/ordersController.js
+++ src/controllers/ordersController.js
@@ -43,4 +43,22 @@ router.post('/', auth, async (req, res) => {
     }
 })
 
+router.post('/pay-order/:id', auth, async (req, res) => {
+    try {
+        const order = await queryHandler.findById(req.params.id)
+
+        if(order.userId != req.user._id) {
+            throw new Error('You are not the owner of the order!')
+        }
+
+        const updatedOrder = await commandHandler.payOrder(req.params.id, order.totalAmount, req.body)
+
+        res.status(200).json(updatedOrder)
+    } catch (err) {
+        res.status(400).json({
+            message: err.message
+        })
+    }
+})
+
 module.exports = router
