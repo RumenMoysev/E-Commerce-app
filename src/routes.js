383e51da27ec1b445bd84df3577c0bb9198de330
RumenMoysev <rumoysev@gmail.com>
2024-06-24 13:30:43 +0300

    Add /orders path and ordersController

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 const userController = require('./controllers/userController.js')
+const ordersController = require('./controllers/ordersController.js')
 
 router.use('/users', userController)
+router.use('/orders', ordersController)
 
 module.exports = router
