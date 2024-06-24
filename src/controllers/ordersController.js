9de5ca0c80ce23a5c0d8133f3fcdd6e2ca7a39a0
RumenMoysev <rumoysev@gmail.com>
2024-06-24 15:32:54 +0300

    Add ways to search for orders

--- src/controllers/ordersController.js
+++ src/controllers/ordersController.js
@@ -2,11 +2,45 @@ const router = require('express').Router()
 
 const { auth } = require('../middlewares/authMiddleware.js')
 const commandHandler = require('../handlers/commandHandler.js')
+const queryHandler = require('../handlers/queryHandler.js')
+
+router.get('/find-by-user-id', auth, async (req, res) => {
+    try {
+        const order = await queryHandler.findByUserId(req.user._id)
+
+        res.status(200).json(order)
+    } catch (err) {
+        res.status(400).json({
+            message: err.message
+        })
+    }
+})
+
+router.get('/:id', auth, async (req, res) => {
+    try {
+        const order = await queryHandler.findById(req.params.id)
+        if (order.userId != req.user._id) {
+            throw new Error('You are not the owner of the order!')
+        }
+
+        res.status(200).json(order)
+    } catch (err) {
+        res.status(400).json({
+            message: err.message
+        })
+    }
+})
 
 router.post('/', auth, async (req, res) => {
-    const order = await commandHandler.createOrder(req.body)
+    try {
+        const order = await commandHandler.createOrder(req.body)
 
-    res.status(200).send(order)
+        res.status(200).json(order)
+    } catch (err) {
+        res.status(400).json({
+            message: err.message
+        })
+    }
 })
 
 module.exports = router
